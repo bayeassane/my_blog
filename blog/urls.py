52092005315f1c40ab521cc5bdefc8c16db100fc
bayeassane <bayikeurguidiop1995@gmail.com>
2020-04-17 00:02:19 +0000

    ajout des articles pages accueil et page blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'blog'
 urlpatterns = [
-
+    path('', views.listing, name='posts'),
+    path('post/<int:id>', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Post
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html')
+    posts = Post.objects.order_by('-title')[:2]
+    return render(request, 'blog/index.html', {'posts': posts})
+
+def listing(request):
+    posts = Post.objects.order_by('-title')
+    return render(request, 'blog/listing.html', {'posts': posts})
+
+def detail(request, id):
+    post = get_object_or_404(Post, pk=id)
+    return render(request, 'blog/detail.html', {'post': post})
